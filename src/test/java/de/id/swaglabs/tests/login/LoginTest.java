be16aeab8e8816e37e26bff5ad0e874196b4e964
idobrowolskij <ilja.dobr@gmail.com>
2025-12-14 20:31:06 +0100

    fixed LoginTest with new structure

--- src/test/java/de/id/swaglabs/tests/login/LoginTest.java
+++ src/test/java/de/id/swaglabs/tests/login/LoginTest.java
@@ -2,20 +2,27 @@ package de.id.swaglabs.tests.login;
 
 import de.id.swaglabs.config.TestConfig;
 import de.id.swaglabs.core.BaseTest;
+import de.id.swaglabs.pages.LoginPage;
+import de.id.swaglabs.pages.ProductsPage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LoginTest extends BaseTest {
 
+    private LoginPage loginPage;
+
     @BeforeEach
     void setup() {
+        loginPage = new LoginPage(page);
         loginPage.open();
     }
 
     @Test
     void shouldLoginWithValidCredentials() {
         loginPage.login(TestConfig.standardUser(), TestConfig.password());
+        ProductsPage productsPage = new ProductsPage(page);
+        assertTrue(productsPage.isAt(), "Expected to be on inventory page after login");
     }
 
     @Test
